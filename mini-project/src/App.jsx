4adfed613d2153dd0deaf13185f96c4120dfdb89
rinehaxor <rinehaxor@users.noreply.github.com>
2023-05-07 00:54:38 +0700

    fix auth protected

--- mini-project/src/App.jsx
+++ mini-project/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/search" element={<MovieList />} />
-            <Route path="/user" element={<UserPage />} />
+            <Route path="/user" element={<ProtectedUser />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="*" element={<h1>Oppss error page 404</h1>} />
