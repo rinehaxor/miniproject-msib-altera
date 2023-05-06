f0a1673e60ddb43fa789bd5d42603cbe08692f3a
rinehaxor <rinehaxor@users.noreply.github.com>
2023-05-06 20:38:36 +0700

    add Image and Route

--- mini-project/src/App.jsx
+++ mini-project/src/App.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import MovieList from './compents/MovieList';
+import User from './compents/User';
+import { useAuth } from './hook/useAuth';
+import { Navigate } from 'react-router-dom';
+import { AuthProvider } from './hook/useAuth';
+import UserPage from './pages/UserPage';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Beranda</Link>
-            </li>
-            <li>
-              <Link to="/daftar-film">Daftar Film</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/daftar-film" element={<MovieList />} />
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/search" element={<MovieList />} />
+            <Route path="/user" element={<UserPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="*" element={<h1>Oppss error page 404</h1>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
+}
+
+function ProtectedUser() {
+  const { currentUser } = useAuth();
+  return currentUser ? <User /> : <Navigate to="/login" replace />;
 }
 
 export default App;
